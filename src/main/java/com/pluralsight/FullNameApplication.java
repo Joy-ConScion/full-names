c811973dccf896d9b75362131f2b63f4db9391f6
Joy-ConScion <chris.torres2001@outlook.com>
2026-04-20 00:25:24 -0500

    feat: snowball name pieces

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -1,4 +1,35 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FullNameApplication {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Provide first name: ");
+        String firstName = input.nextLine();
+        System.out.println("Provide last name: ");
+        String lastName = input.nextLine();
+        System.out.println("Provide middle name (if N/A, then skip): ");
+        String middleInitial = input.nextLine();
+        System.out.println("Provide suffix (if N/A, then skip): ");
+        String suffix = input.nextLine();
+        String fullName = "";
+        fullName = firstName;
+
+        if (!middleInitial.isEmpty()) {
+            fullName = fullName + " " + middleInitial;
+        }
+        if (!lastName.isEmpty()) {
+            fullName = fullName + " " + lastName;
+        }
+        if (!suffix.isEmpty()) {
+            fullName = fullName + ", " + suffix;
+        }
+        System.out.print(fullName);
+
+    }
+
+
 }
--- src/main/java/com/pluralsight/NamesLogbook.java
+++ src/main/java/com/pluralsight/NamesLogbook.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
 
 public class NamesLogbook {
+    public static void main(String[] args) {
+
+        String firstName = "";
+        String middleInitial = "";
+        String lastName = "";
+
+    }
 }
